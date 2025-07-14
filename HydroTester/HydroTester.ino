2b7e14eaf3895b3f322f3b029f37a60ebd128efb
Austin Hendrix <namniart@gmail.com>
2025-07-14 02:04:43 -0700

    Menu mostly working

--- HydroTester/HydroTester.ino
+++ HydroTester/HydroTester.ino
@@ -1,6 +1,8 @@
+#include <ezButton.h>
 #include <SoftwareSerial.h>
 #include <Adafruit_Thermal.h>
 #include <DualVNH5019MotorShield.h>
+
 
 #include "Lcd.h"
 
@@ -39,10 +41,11 @@ LCD lcd(LCD_port);
 SoftwareSerial printerPort(18, 17);
 Adafruit_Thermal printer(&printerPort);
 
-double step_psi = 10.0f;
-uint8_t step_hold_minutes = 5;
-double loss_threshold_psi = 1.0f;
-double test_pressure_psi = 100.0f;
+float test_pressure_psi = 100.0f;
+uint16_t test_hold_minutes = 15;
+float step_psi = 10.0f;
+uint16_t step_hold_minutes = 5;
+float loss_threshold_psi = 1.0f;
 uint16_t release_time_minutes = 60;
 
 // Encoder handling
@@ -69,7 +72,7 @@ void ISR_encoderChange() {
   last_time = millis();
 }
 
-void menuLine(MenuSelection m, bool selected, bool active) {
+void menuLine(MenuSelection m, bool selected, bool active, bool ser) {
   constexpr char activeChar = '*';
   constexpr char selectChar = '>';
   constexpr char idleChar = ' ';
@@ -77,6 +80,10 @@ void menuLine(MenuSelection m, bool selected, bool active) {
   char buffer[bufferSize+1];
   const int8_t idx = (int8_t)m;
   int p = 0;
+
+#define PRINT(...) do {\
+    p += snprintf(buffer+p, bufferSize+1-p, __VA_ARGS__);\
+  } while(0)
 
   // Menu:
   //  1. Start
@@ -92,36 +99,44 @@ void menuLine(MenuSelection m, bool selected, bool active) {
   //  11. Self test
   // buffer[0] = (active?activeChar:(selected?selectChar:idleChar));
   // p = 1;
-  p = snprintf(buffer, bufferSize,
+  PRINT(
     "%c%2d. ",
     (active?activeChar:(selected?selectChar:idleChar)),
     idx+1);
 
   switch (m) {
     case MenuSelection::START:
-      p += snprintf(buffer+p, bufferSize-p, "Start");
+      PRINT("Start");
       break;
     case MenuSelection::MAX:
-      p += snprintf(buffer+p, bufferSize-p, "Max %3d",
-      (int)test_pressure_psi);
+      PRINT("Max %3d", (int)test_pressure_psi);
       break;
     case MenuSelection::MAX_HOLD:
-      p += snprintf(buffer+p, bufferSize-p, "Max hold %3d",
-      (int)test_pressure_psi);
+      PRINT("Max hold %3d", test_hold_minutes);
       break;
     case MenuSelection::STEP:
-      p += snprintf(buffer+p, bufferSize-p, "Step %2d",
-      (int)step_psi);
+      PRINT("Step %2d", (int)step_psi);
       break;
     case MenuSelection::STEP_HOLD:
-      p += snprintf(buffer+p, bufferSize-p, "Step hold %2d",
-      step_hold_minutes);
+      PRINT("Step hold %2d", step_hold_minutes);
       break;
     case MenuSelection::RELEASE:
-      p += snprintf(buffer+p, bufferSize-p, "Release %3d",
-      release_time_minutes);
+      PRINT("Release %3d", release_time_minutes);
       break;
-    default:
+    case MenuSelection::LOSS_THRESH:
+      PRINT("Threshold %d", (int)loss_threshold_psi);
+      break;
+    case MenuSelection::MANUAL_FILL:
+      PRINT("Manual Fill");
+      break;
+    case MenuSelection::MANUAL_BLEED:
+      PRINT("Manual Bleed");
+      break;
+    case MenuSelection::MANUAL_PUMP:
+      PRINT("Manual Pump");
+      break;
+    case MenuSelection::SELF_TEST:
+      PRINT("Self Test");
       break;
   }
   // Fill remainder of buffer with spaces to clear display.
@@ -130,37 +145,54 @@ void menuLine(MenuSelection m, bool selected, bool active) {
   }
 
   digitalWrite(13, HIGH);
-  lcd.write(buffer);
+  lcd.write((uint8_t*)(buffer), bufferSize);
   digitalWrite(13, LOW);
+  if (ser) {
+    Serial.write((uint8_t*)(buffer), bufferSize);
+    Serial.print("% ");
+    Serial.println(bufferSize);
+  }
 }
 
 void updateDisplay() {
   static int8_t last_counter = 0;
   static int8_t last_button = 0;
   static MenuSelection selection = MenuSelection::START;
-  bool active = false;
+  static bool active = false;
+  bool ser = false;
 
   int8_t button = !digitalRead(BUTTON_PIN);
-  // Snapshot selection state so it doesn't change while we're drawing the display.
-  // const MenuSelection selection = ::selection;
+
+  if (button && button != last_button) {
+    active = !active;
+  }
+
+  last_button = button;
 
   // Update menu selection.
   int8_t counter_delta = counter - last_counter;
   last_counter = counter;
 
-  if (button) {
-    if (button != last_button) {
-      active = !active;
-    }
+  if (counter_delta != 0) {
+    ser = true;
   }
-  last_button = button;
 
   if (active) {
     switch (selection) {
+      case MenuSelection::MAX:
+        test_pressure_psi += counter_delta;
+        break;
+      case MenuSelection::MAX_HOLD:
+        test_hold_minutes += counter_delta;
+        break;
       case MenuSelection::STEP:
         step_psi += counter_delta;
         break;
+      case MenuSelection::STEP_HOLD:
+        step_hold_minutes += counter_delta;
+        break;
       default:
+        ser = false;
         break;
     }
   } else {
@@ -173,15 +205,16 @@ void updateDisplay() {
   digitalWrite(13, LOW);
 
   if (selection < (MENU_ITEMS-1)) {
-    menuLine(selection, true, active);
-    menuLine(static_cast<MenuSelection>(selection+1), false, false);
+    menuLine(selection, true, active, ser);
+    menuLine(static_cast<MenuSelection>(selection+1), false, false, ser);
   } else {
-    menuLine(static_cast<MenuSelection>(selection-1), false, false);
-    menuLine(selection, true, active);
+    menuLine(static_cast<MenuSelection>(selection-1), false, false, ser);
+    menuLine(selection, true, active, ser);
   }
 }
 
 void setup() {
+  Serial.begin(115200);
   pinMode(13, OUTPUT); // LED pin as output.
 
   // Configure encoder pins as inputs.
